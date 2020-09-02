0213007d46178dbd38351e52b046a98ff09ca373
Aditya <aditya.sharma5645@gmail.com>
2020-09-02 17:13:24 +0530

    Ugly Number

---
+++ Dynamic Programming/ugly-number-1.cpp
@@ -0,0 +1,15 @@
+// 263. Ugly Number
+
+class Solution {
+public:
+    bool isUgly(int num) {
+
+    for(int i = 2; i<=5 && num; i++)
+    {
+        while(num%i==0)
+            num/=i;
+    }
+
+       return num == 1;
+    }
+};
