0fabb1e759636f0d947033ffc722272fccd11a55
Aditya <aditya.sharma5645@gmail.com>
2020-11-08 20:34:27 +0530

    Divide & Conquer

---
+++ Divide & Conquer/sum-of-middle-elements.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(0);
+  cin.tie(0);
+	cout.tie(0);
+
+	ll t;
+	cin >> t;
+
+	while(t--)
+  {
+		  ll n;
+      cin >> n;
+
+      ll a[n];
+      ll b[n];
+
+      for(ll i = 0; i<n; i++)
+        cin >> a[i];
+
+      for(ll i =0; i<n; i++)
+        cin >> b[i];
+
+      ll temp = 0;
+
+      while( (i+j) < n/2 )
+      {
+
+
+
+
+
+      }
+
+	}
+
+}
